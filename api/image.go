6eeb09d47e021cae1ee26695ddbc3ffca371253b
modeckrus <modeckrus.official.page@gmail.com>
2023-03-29 21:00:32 +0300

    fixes

--- api/image.go
+++ api/image.go
@@ -1,1 +1,22 @@
 package api
+
+import "github.com/gin-gonic/gin"
+
+type ImageAPI struct{}
+
+func NewImageApi() ImageAPI {
+	return ImageAPI{}
+}
+
+func ImageGroup(r *gin.Engine, url string) {
+	api := NewImageApi()
+	group := r.Group("/api/images")
+	{
+		group.POST("/upload", api.UploadEndPoint)
+		group.GET("/data", api.DataEndPoint)
+		group.GET("/file", api.FileEndPoint)
+		group.POST("/recognize", api.RecognizeEndPoint)
+		group.POST("/save", api.SaveEndPoint)
+	}
+
+}
